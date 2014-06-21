6a77aeee4baf7778c6804fe0b647db8e3741c201
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-06-21 22:25:15 +0900

    Add set-yaml-mode

--- .emacs.d/config/programming/set-yaml-mode.el
+++ .emacs.d/config/programming/set-yaml-mode.el
@@ -1,5 +1,5 @@
 (require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml\\$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
 (add-hook 'yaml-mode-hook
           '(lambda ()
