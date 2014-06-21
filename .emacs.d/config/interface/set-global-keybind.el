224b7aab01bcdaa24c6d1101ae91ecec8514dfaa
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-06-21 22:24:58 +0900

    Update magit keybind

--- .emacs.d/config/interface/set-global-keybind.el
+++ .emacs.d/config/interface/set-global-keybind.el
@@ -1,5 +1,7 @@
 (keyboard-translate ?\C-h ?\C-?)
 (define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+(define-key global-map (kbd "C-c C-s") 'magit-status)
 (define-key global-map (kbd "C-t") 'other-window)
+
 
 (provide 'set-global-keybind)
