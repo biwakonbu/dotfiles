a1851c991bbce8fb23270f80737bf8ccd029f777
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-07-04 11:28:04 +0900

    Update history-token-next-word.fish

--- fish/functions/history-token-next-word.fish
+++ fish/functions/history-token-next-word.fish
@@ -6,7 +6,7 @@ function history-token-next-word
     set -g blacklist (echo '^'$blacklist | gsed -e 's/\s/|/g')'|^ .*'
     set -g complete_list '^'(complete -C$trim_buffer | cut -f1 | ag -v '\n' | egrep -v $blacklist | gsed -e 's/\s/|/g')
     set -g filter (echo $complete_list | gsed 's/-/\\\-/g' | gsed 's/\+/\\\+/g' | gsed -e 's/\s/|/g')
-    set -g words (history | ag "$filter")
+    set -g words (history | ag "$filter" | ag $commandline_buffer)
     if test 0 -lt (count $words)
       set -g token (echo $words[1] | gsed "s/$commandline_buffer//" | cut -d' ' -f1)
       set -l completion_word (echo $words[1] | gsed "s/\\($token\\).*\$/\\1/")' '
