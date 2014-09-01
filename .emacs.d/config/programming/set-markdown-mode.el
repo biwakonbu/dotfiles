7c086844504574bf2d7dc7139e1906ebcc7a272c
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-09-01 18:23:09 +0900

    Update Markdown mode

--- .emacs.d/config/programming/set-markdown-mode.el
+++ .emacs.d/config/programming/set-markdown-mode.el
@@ -3,5 +3,9 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("cask$" . "emacs-lisp-mode"))
+
+(setq markdown-command-needs-filename t)
 
 (provide 'set-markdown-mode)
