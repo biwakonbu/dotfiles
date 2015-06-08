5df708579c7f17ebba43e3b8d205fed5f2421a14
Ryo HIGASHIGAWA <higashigawa@platinum-egg.com>
2015-06-08 17:00:02 +0900

    add docker settings

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,13 @@ set -x GOPATH $HOME/dev/go-workspace
 
 # rbenv
 set rbenv_path $HOME/.rbenv/bin $HOME/.rbenv/shims
-set -gx PATH $rbenv_path $PATH 
+set -gx PATH $rbenv_path $PATH
 rbenv rehash > /dev/null ^&1
+
+# pyenv
+set pyenv_path $HOME/.pyenv/bin $HOME/.pyenv/shims
+set -gx PATH $pyenv_path $PATH
+pyenv rehash > /dev/null ^&1
 
 # cask
 set cask_path $HOME/.cask/bin
@@ -13,3 +18,8 @@ set -gx PATH $cask_path $PATH
 
 # MacPorts
 set -x PATH /opt/local/bin /opt/local/sbin $PATH
+
+# MacOSX Docker ENV
+if which boot2docker > /dev/null
+  docker_init 
+end
